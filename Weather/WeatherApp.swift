dc6662f9c792aa6c1aa8087523d93add0e32dbb4
Alex <alexrozh1988@gmail.com>
2021-10-15 01:29:09 +0300

    Part_1

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alexander Rozhdestvenskiy on 15.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
